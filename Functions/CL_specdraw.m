864ce6cee20c5c9800626f90e9c7b63a836fc52d
braun <mbraun848@gmail.com>
2020-04-26 21:33:47 -0700

    Update CL_specdraw

--- Functions/CL_specdraw.m
+++ Functions/CL_specdraw.m
@@ -3,17 +3,17 @@ function CL_specdraw(~,spec_data_corr,CLspec_plot)
 global CLspectra_permuted ellipseHandle mask_single
 
 mask_single=createMask(ellipseHandle);
-% NumberSpectraPoints=nnz(mask_single);
-% fprintf('%g averaged points in the spectrum\n',NumberSpectraPoints)
-spec_selected=zeros(size(spec_data_corr));
-% kk=1;
+spec_selected=zeros(nnz(mask_single),size(spec_data_corr,3));
+kk=1;
 for ii=1:size(mask_single,1)
     for jj=1:size(mask_single,2)
-        %         if mask_single(ii,jj)==0
-        spec_selected(ii,jj,:)=spec_data_corr(ii,jj,:).*(mask_single(ii,jj));
+        if mask_single(ii,jj)==0
+        else spec_selected(kk,:)=spec_data_corr(ii,jj,:).*(mask_single(ii,jj));
+            kk=kk+1;
+        end
     end
 end
-CLspectra=mean(mean(spec_selected,1),2);
-CLspectra_permuted=(permute(CLspectra,[3 2 1]));
+CLspectra=mean(spec_selected,1);
+CLspectra_permuted=(permute(CLspectra,[2 1]));
 set(CLspec_plot, 'YData', CLspectra_permuted);
 end
